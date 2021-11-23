3913653b40383648e65c88dc875d946b1e62abf5
Francisco Santos <francisco.santos@ydata.ai>
2021-11-23 17:36:00 +0000

    base processor and metadata object

--- src/ydata_synthetic/preprocessing/base_processor.py
+++ src/ydata_synthetic/preprocessing/base_processor.py
@@ -1,9 +1,17 @@
 from typing import List, Union
 
-from numpy import concatenate, ndarray, split, zeros
-from pandas import concat, DataFrame
+from abc import ABC, abstractmethod
+from collections import namedtuple
+from typing import List, Optional
+
+from numpy import ndarray
+from pandas import DataFrame, Series
 from sklearn.base import BaseEstimator, TransformerMixin
 from typeguard import typechecked
+
+ProcessorInfo = namedtuple("ProcessorInfo", ["numerical", "categorical"])
+PipelineInfo = namedtuple("PipelineInfo", ["pipeline_name", "feat_names_in", "feat_names_out"])
+
 
 @typechecked
 class BaseProcessor(BaseEstimator, TransformerMixin):
@@ -34,7 +42,68 @@ class BaseProcessor(BaseEstimator, TransformerMixin):
         self.col_order_ = None
         self.pos_idx = pos_idx
 
-    def fit(self, X: DataFrame):
+        self._col_transform_info = None  # Metadata object mapping inputs/outputs of each pipeline
+
+    @property
+    def num_pipeline(self) -> BaseEstimator:
+        """Returns the pipeline applied to numerical columns."""
+        return self._num_pipeline
+
+    @property
+    def cat_pipeline(self) -> BaseEstimator:
+        """Returns the pipeline applied to categorical columns."""
+        return self._cat_pipeline
+
+    @property
+    def types(self) -> Series:
+        """Returns a Series with the dtypes of each column in the fitted DataFrame."""
+        return self._types
+
+    @property
+    def col_transform_info(self) -> PipelineInfo:
+        """Returns a PipelineInfo object specifying input/output feature mappings of this processor's pipelines."""
+        self._check_is_fitted()
+        if self._col_transform_info is None:
+            self._col_transform_info = self.__create_metadata_synth()
+        return self._col_transform_info
+
+    def __create_metadata_synth(self):
+        num_info = None
+        cat_info = None
+        # Numerical ls named tuple
+        if self.num_cols:
+            num_info = PipelineInfo(
+                                    'numeric',
+                                    self.num_pipeline.feature_names_in,
+                                    self.num_pipeline.get_feature_names_out())
+        # Categorical ls named tuple
+        if self.cat_cols:
+            cat_info = PipelineInfo(
+                                    'categorical',
+                                    self.cat_pipeline.feature_names_in,
+                                    self.cat_pipeline.get_feature_names_out())
+        return ProcessorInfo(num_info, cat_info)
+
+    def _check_is_fitted(self):
+        """Checks if the processor is fitted by testing the numerical pipeline.
+        Raises NotFittedError if not."""
+        if self._num_pipeline is None:
+            raise NotFittedError("This data processor has not yet been fitted.")
+
+    def _validate_cols(self, x_cols):
+        """Ensures validity of the passed numerical and categorical columns.
+        The following is verified:
+            1) Num cols and cat cols are disjoint sets;
+            2) The union of these sets should equal x_cols;.
+        Assertion errors are raised in case any of the tests fails."""
+        missing = set(x_cols).difference(set(self.num_cols).union(set(self.cat_cols)))
+        intersection = set(self.num_cols).intersection(set(self.cat_cols))
+        assert intersection == set(), f"num_cols and cat_cols share columns {intersection} but should be disjoint."
+        assert missing == set(), f"The columns {missing} of the provided dataset were not attributed to a pipeline."
+
+    # pylint: disable=C0103
+    @abstractmethod
+    def fit(self, X: DataFrame) -> BaseProcessor:
         """Fits the DataProcessor to a passed DataFrame.
         Args:
             X (DataFrame):
